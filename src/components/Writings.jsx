f47a520214629a138cb475e146a0de2aa0489851
arjunsarkar1 <arjunsarkar82@gmail.com>
2026-04-29 02:15:48 -0500

    added public dir

--- src/components/Writings.jsx
+++ src/components/Writings.jsx
@@ -36,13 +36,14 @@ export default function Writings() {
   // PDF mappings
   const pdfMappings = {
     "Optimizing Matrix Operations for Machine Learning: Analysis of AMD's MI100 GPU":
-      "OMPML.pdf",
-    "Bayesian Imitation Learning with Uncertainty Propagation": "BILUP.pdf",
-    "Lessons in Software Safety": "SoftwareSafety.pdf",
-    "Teamwork Effectiveness": "Teamwork.pdf",
-    "AI Ownership": "AIOwnership.pdf",
-    "Ethics in Surveillance Capitalism": "Capitalism.pdf",
-    "Addressing Cyber Attacks": "CyberAttacks.pdf",
+      "public/OMPML.pdf",
+    "Bayesian Imitation Learning with Uncertainty Propagation":
+      "public/BILUP.pdf",
+    "Lessons in Software Safety": "public/SoftwareSafety.pdf",
+    "Teamwork Effectiveness": "public/Teamwork.pdf",
+    "AI Ownership": "/AIOwnership.pdf",
+    "Ethics in Surveillance Capitalism": "public/Capitalism.pdf",
+    "Addressing Cyber Attacks": "public/CyberAttacks.pdf",
   };
 
   const handlePDFClick = (title) => {
